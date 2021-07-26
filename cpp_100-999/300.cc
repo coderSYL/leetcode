ec6ab0a5fc31b8bd3da9041be576705df33fb9f7
syl <public.syl@outlook.com>
2021-07-26 22:58:35 +0800

    老方法:两次遍历 -> 新方法:一次遍历，一次二分

--- cpp_100-999/300.cc
+++ cpp_100-999/300.cc
@@ -1,27 +1,29 @@
 // C++
 // leetcode 300
 // https://leetcode-cn.com/problems/longest-increasing-subsequence/
-// dp[i]用来存储以nums[i]为结尾的最长子序列的大小
-// 遍历求一遍dp[0] ~ dp[size-1]
-// max(dp[0], ... , dp[size-1])就是答案
+// dp[i]用来存储以nums[i]为结尾的最长子序列的大小，遍历一遍求得
+// g[i] 用来存储长度为i的递增子序列的最小的结尾，
  
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-    	int size =nums.size(), ret = 1, temp = 0;
-    	int dp[size];
-    	dp[0] = 1;
-    	for(int i = 1; i<size; i++){
-    		temp = nums[i];
-    		dp[i] = 1;
-    		for(int j =0; j<i; j++){
-    			if(temp>nums[j] && dp[j]>=dp[i]){
-    				dp[i] = dp[j]+1;
-    			}
-    		}
-    		if(dp[i]>ret)
-    			ret =dp[i];
-    	}
-    	return ret;
+        int size =nums.size(), ret = 1, temp = 0;
+        int dp[size];
+        vector<int> g(size+1, INT_MAX);
+        for(int i = 0; i<size; i++){
+            int l = 0, r = size;
+            while(l < r) {
+                int mid = (l + r + 1) / 2;
+                if(g[mid] < nums[i])
+                    l = mid;
+                else
+                    r = mid - 1;
+            }
+            int clen = r + 1;
+            dp[i] = clen;
+            g[clen] = min(g[clen], nums[i]);
+            ret = max(ret, clen);
+        }
+        return ret;
     }
 };
